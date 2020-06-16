612015809ab536f17f8e1890371f55a500969e11
Ronaldo Macapobre <RonaldoM@magenic.com>
2020-06-16 12:39:58 +0800

    Updates

---
+++ precache-manifest.0da43cb1eefa03d2ffca1f046d822376.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28a2c983798bd1b7e95014b62e1c1b8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9978c51085216025a30d",
+    "url": "/monsters-rolodex/static/css/main.48985317.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9978c51085216025a30d",
+    "url": "/monsters-rolodex/static/js/main.97d1b91c.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
